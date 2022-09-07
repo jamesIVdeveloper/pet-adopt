55e7cc4ce0093bdd60078434fdb87cb83ba0e701
James Murphy <jamesivdeveloper@gmail.com>
2022-09-07 11:00:00 -0500

    add SearcParams component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Killian" animal="Cat" breed="Bombay"></Pet>
-      <Pet name="Angel" animal="Dog" breed="Golden Retriever"></Pet>
-      <Pet name="Mac" animal="Dog" breed="Golden Doodle"></Pet>
+      <SearchParams />
     </div>
   );
 };
